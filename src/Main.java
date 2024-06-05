d2d0548cc32724a1a1d81921490811fbbc7b7123
goldovskaya-m <ariya-aaa@yandex.ru>
2024-06-05 08:00:06 +0300

    Внесла изменения после проверки

--- src/Main.java
+++ src/Main.java
@@ -5,29 +5,44 @@ public class Main {
         System.out.println("Homework9");
         System.out.println("Задача 1");
         double[] monday = {5, 6, 7, 8, 9};
-        int SUM = 0;
+        double SUM = 0;
         for (int i = 0; i < monday.length; i++) {
             SUM += monday[i];
         }
         System.out.printf("Сумма трат за месяц составила %s рублей %n", SUM);
         System.out.println("задача 2");
-        double minMonday = monday[0] / 4;
-        double maxMonday = monday[0] / 4;
+
+        //оставила себе, подумать
+        //double minMonday = monday[0] / 4;
+        //double maxMonday = monday[0] / 4;
+        //for (int i = 0; i < monday.length; i++) {
+           // if (monday[i] / 4 > minMonday) {
+                //maxMonday = monday[i] / 4;
+            //}
+        //}
+        //System.out.println("Максимальные расходы за неделю " + maxMonday);
+        //System.out.println("Минимальный расход за неделю " + minMonday);
+        //как просил исправить Герман
+        double minMonday2 = monday[0];
+        double maxMonday2 = monday[0];
         for (int i = 0; i < monday.length; i++) {
-            if (monday[i] / 4 > minMonday) {
-                maxMonday = monday[i] / 4;
+            if (monday[i] > minMonday2) {
+                maxMonday2 = monday[i];
             }
         }
-        System.out.println("Максимальные расходы за месяц " + maxMonday);
-        System.out.println("Минимальный расход за месяц " + minMonday);
+        System.out.println("Максимальные расходы за месяц " + maxMonday2);
+        System.out.println("Минимальный расход за месяц " + minMonday2);
+
         System.out.println("Задача 3");
-        int SUM2 = 0;
+        double SUM2 = 0;
         for (int i = 0; i < monday.length; i++) {
             SUM2 += monday[i];
         }
-        double averageWeek = ((double) SUM2 / monday.length) / 4;
-        System.out.printf("Средняя сумма трат за месяц в недели " +
+        double averageWeek = ((double) SUM2 / monday.length) ;
+        System.out.printf("Средняя сумма трат за месяц  35/5 = " +
                 "составила %s рублей%n ", averageWeek);
+
+
         System.out.println("Задача 4");
         char[] reverseFullName = {'n', 'a', 'v', 'I', ' ', 'v', 'o', 'n', 'a', 'v', 'I'};
         char temp;
@@ -46,8 +61,18 @@ public class Main {
             reverseFullName2[reverseFullName2.length -1 -i] = temp;
         }
         System.out.println(Arrays.toString(reverseFullName2));
+        // Исправлено, как просил Герман
 
+        for (int i = 0; i < reverseFullName.length; i++) {
+            System.out.print(reverseFullName[i]);
+            if (i != reverseFullName.length - 1) {
+                System.out.print(",");
+            }
         }
+        System.out.println();
+
+
+    }
     }
 
 
