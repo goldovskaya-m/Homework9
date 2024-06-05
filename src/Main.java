2fceb0196c47d0ccb1b065a809729828ad1e2af4
goldovskaya-m <ariya-aaa@yandex.ru>
2024-06-05 22:27:13 +0300

    Нужна помощь=) Во второй задаче, выводится первое и последнее число цикла. Мах и мин не выводится. Возможно дело в скобках фигурных? К 4 задаче коментарий про for(int i = reverseFullName.length - 1; i >= 0; i--) не поняла. И так выводит Иванов Иван,работает?

--- src/Main.java
+++ src/Main.java
@@ -4,42 +4,43 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Homework9");
         System.out.println("Задача 1");
-        double[] monday = {5, 6, 7, 8, 9};
-        double SUM = 0;
+        double[] monday = {3, 5, 7, 9, 8};
+        double sum = 0;
         for (int i = 0; i < monday.length; i++) {
-            SUM += monday[i];
+            sum += monday[i];
         }
-        System.out.printf("Сумма трат за месяц составила %s рублей %n", SUM);
+        System.out.printf("Сумма трат за месяц составила %s рублей %n", sum);
         System.out.println("задача 2");
 
-        //оставила себе, подумать
-        //double minMonday = monday[0] / 4;
-        //double maxMonday = monday[0] / 4;
-        //for (int i = 0; i < monday.length; i++) {
-           // if (monday[i] / 4 > minMonday) {
-                //maxMonday = monday[i] / 4;
-            //}
-        //}
-        //System.out.println("Максимальные расходы за неделю " + maxMonday);
-        //System.out.println("Минимальный расход за неделю " + minMonday);
-        //как просил исправить Герман
-        double minMonday2 = monday[0];
-        double maxMonday2 = monday[0];
+        double minMonday = monday[0];
+        double maxMonday = monday[0];
         for (int i = 0; i < monday.length; i++) {
-            if (monday[i] > minMonday2) {
-                maxMonday2 = monday[i];
+
+            if (monday[i] > minMonday) {
+                maxMonday = monday[i];
+                if (monday[i] < maxMonday) {
+                    minMonday = monday[i];
+                }
+
+
+
+
+
+
+
+
             }
         }
-        System.out.println("Максимальные расходы за месяц " + maxMonday2);
-        System.out.println("Минимальный расход за месяц " + minMonday2);
+        System.out.println("Максимальные расходы за месяц " + maxMonday);
+        System.out.println("Минимальный расход за месяц " + minMonday);
 
         System.out.println("Задача 3");
-        double SUM2 = 0;
+        double sum2 = 0;
         for (int i = 0; i < monday.length; i++) {
-            SUM2 += monday[i];
+            sum2 += monday[i];
         }
-        double averageWeek = ((double) SUM2 / monday.length) ;
-        System.out.printf("Средняя сумма трат за месяц  35/5 = " +
+        double averageWeek = ((double) sum2 / monday.length);
+        System.out.printf("Средняя сумма трат за месяц = " +
                 "составила %s рублей%n ", averageWeek);
 
 
@@ -55,10 +56,10 @@ public class Main {
         // 2 вариант Задача 4
         char[] reverseFullName2 = {'n', 'a', 'v', 'I', ' ', 'v', 'o', 'n', 'a', 'v', 'I'};
         char temp2;
-        for (int i = 0; i < reverseFullName2.length /2 ; i++) {
+        for (int i = 0; i < reverseFullName2.length / 2; i++) {
             temp = reverseFullName2[i];
             reverseFullName2[i] = reverseFullName2[reverseFullName2.length - 1 - i];
-            reverseFullName2[reverseFullName2.length -1 -i] = temp;
+            reverseFullName2[reverseFullName2.length - 1 - i] = temp;
         }
         System.out.println(Arrays.toString(reverseFullName2));
         // Исправлено, как просил Герман
@@ -73,6 +74,6 @@ public class Main {
 
 
     }
-    }
+}
 
 
